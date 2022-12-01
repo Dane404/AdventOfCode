d931f389fe279baceb7afe5032931a5ff0efb5cc
Danijel Pejic <danijelpejic15@gmail.com>
2022-12-01 20:01:44 +0100

    Seperated Level 1/1 and 1/2

--- AdventOfCode2022/src/main/kotlin/Main.kt
+++ AdventOfCode2022/src/main/kotlin/Main.kt
@@ -3,13 +3,33 @@ import java.util.LinkedList
 
 fun main(args: Array<String>) {
     var lines = getLines("src/main/kotlin/input.txt");
-    println("-------------------Level 1--------------------")
+    println("-------------------Level 1.1--------------------")
+    println("Most Calories: "+getElfWithMostCalories(lines));
+    println("----------------------------------------------")
+    println("-------------------Level 1.2--------------------")
     println("Sum of top 3 elfs calories: "+getTopThreeElfWithMostCalories(lines));
     println("----------------------------------------------")
-
-
 }
-
+fun getElfWithMostCalories(lines:LinkedList<String>):Int
+{
+    var mostCalories = 0;
+    var currCount=0;
+    for(line: String in lines)
+    {
+        if(line.equals(""))
+        {
+            if(currCount>mostCalories)
+            {
+                mostCalories=currCount;
+            }
+            currCount=0;
+        }
+        else{
+            currCount+=line.toInt();
+        }
+    }
+    return mostCalories;
+}
 fun getTopThreeElfWithMostCalories(lines:LinkedList<String>):Int
 {
     var listOfCalories = LinkedList<Int>();
@@ -25,8 +45,8 @@ fun getTopThreeElfWithMostCalories(lines:LinkedList<String>):Int
             currCount+=line.toInt();
         }
     }
-    listOfCalories.sort();
-    return listOfCalories.get(listOfCalories.size-1)+listOfCalories.get(listOfCalories.size-2)+listOfCalories.get(listOfCalories.size-3);
+    listOfCalories.sort();//sorts the array in asc order
+    return listOfCalories.get(listOfCalories.size-1)+listOfCalories.get(listOfCalories.size-2)+listOfCalories.get(listOfCalories.size-3);//the last three items of the list are the biggest
 }
 
 fun getLines(path: String):LinkedList<String>
