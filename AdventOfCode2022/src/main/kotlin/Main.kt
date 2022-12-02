e60b9a088f682e6838565a769799572a6b77b9ad
Danijel Pejic <danijelpejic15@gmail.com>
2022-12-02 09:23:03 +0100

    Finished Day 2

--- AdventOfCode2022/src/main/kotlin/Main.kt
+++ AdventOfCode2022/src/main/kotlin/Main.kt
@@ -2,14 +2,88 @@ import java.io.File
 import java.util.LinkedList
 
 fun main(args: Array<String>) {
-    var lines = getLines("src/main/kotlin/input.txt");
+    var linesDay1 = getLines("src/main/kotlin/input.txt");
     println("-------------------Level 1--------------------")
-    println("Sum of top 3 elfs calories: "+getTopThreeElfWithMostCalories(lines));
+    println("Sum of top 3 elfs calories: "+getTopThreeElfWithMostCalories(linesDay1));
     println("----------------------------------------------")
-
+    var linesDay2 = getLines("src/main/kotlin/input_2.txt");
+    println("-------------------Level 2.1--------------------")
+    println("Score: "+calculateScorePart1(linesDay2));
+    println("----------------------------------------------")
+    println("-------------------Level 2.2--------------------")
+    println("Score: "+calculateScorePart2(linesDay2));
+    println("----------------------------------------------")
 
 }
 
+
+const val WINNING_POINTS=6;
+const val DRAW_POINTS=3;
+const val LOSING_POINTS=0;
+fun calculateScorePart1(lines:LinkedList<String>):Int{
+    var winningHands= HashMap<String,String>();
+    winningHands.put("A","Y");
+    winningHands.put("B","Z");
+    winningHands.put("C","X");
+    var score = 0;
+    for(line:String in lines)
+    {
+        val column = line.split(" ");
+        var shapePoints=(column[1].toCharArray()[0]-'X')+1;
+        score += if(winningHands[column[0]].equals(column[1])) {
+            WINNING_POINTS;
+        } else if((column[0].toCharArray()[0]-'A'+1)==shapePoints) {
+            DRAW_POINTS;
+        } else{
+            LOSING_POINTS;
+        }
+        score+=shapePoints;
+    }
+    return score;
+}
+fun calculateScorePart2(lines:LinkedList<String>):Int{
+    var winningHands= HashMap<String,String>();
+    winningHands.put("A","Y");
+    winningHands.put("B","Z");
+    winningHands.put("C","X");
+    var score = 0;
+    for(line:String in lines)
+    {
+        val column = line.split(" ");
+        var shapePoints=(column[0].toCharArray()[0]-'A'+1);
+
+        println(line)
+
+        if(column[1]=="X") {
+            score+=LOSING_POINTS;
+            if(shapePoints-1==0)
+            {
+                shapePoints=3;
+            }
+            else{
+                shapePoints--;
+            }
+        } else if(column[1]=="Y") {
+            score+=DRAW_POINTS;
+        } else if(column[1]=="Z"){
+            score+=WINNING_POINTS;
+            if(shapePoints+1>3)
+            {
+                shapePoints=1;
+            }
+            else{
+                shapePoints++;
+            }
+        }
+        println(shapePoints)
+        println(score)
+
+        score+=shapePoints;
+        println(score)
+        println();
+    }
+    return score;
+}
 fun getTopThreeElfWithMostCalories(lines:LinkedList<String>):Int
 {
     var listOfCalories = LinkedList<Int>();
